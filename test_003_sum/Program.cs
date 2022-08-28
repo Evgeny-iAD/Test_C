7638abdd9a8b5f724e08560f91e2ffdc820aeba3
iAD <iadmail1987@gmail.com>
2022-08-28 21:47:20 +0300

    "test"

--- test_003_sum/Program.cs
+++ test_003_sum/Program.cs
@@ -3,7 +3,7 @@
 int number_A  = new Random().Next(1, 10); // 1 2 3 .. 9
 Console.WriteLine(number_A);
 int number_B  = new Random().Next(1, 10);
-Console.WriteLine(number_B);
+Console.WriteLine(number_Bq);
 Console.WriteLine(number_A + number_B);
 
 
--- test_004_del/Program.cs
+++ test_004_del/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int number_A  = 3;
-int number_B  = 5;
-Console.WriteLine(number_A + number_B);
+double number_A  = 3;
+double number_B  = 5;
+Console.WriteLine(number_A / number_B);

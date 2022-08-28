4dabdd599e4267a4731ea092a94210780b1b548c
iAD <iadmail1987@gmail.com>
2022-08-28 21:53:49 +0300

    работа с приветствиями test_005

--- test_005_priv/Program.cs
+++ test_005_priv/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-double number_A  = 3;
-double number_B  = 5;
-Console.WriteLine(number_A / number_B);
+Console.Write("Введите имя пользователя: ");
+string user_name = Console.ReadLine();
+if (user_name.ToLower() == "Мария")   //ToLower приписка для игнора регистра
+{
+Console.Write("ПШла работать! ");
+}
+else
+{
+Console.Write("Добрый день!");
+Console.WriteLine(user_name);
+}

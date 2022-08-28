8c7935ef7f7de03724be5ad42770bc25f88248e0
iAD <iadmail1987@gmail.com>
2022-08-29 00:16:18 +0300

    домашнее задание 1 семинар

---
+++ test_009_dz_1_sem/8.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите любое целое число больше 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while (count <= number)
+{
+ Console.Write(count);
+ Console.Write(" ");
+ count = count + 2;
+}
+
+
+
+

be9cf1131eb4d439a94435c3b14649c4f96c9f51
iAD <iadmail1987@gmail.com>
2022-08-29 00:20:50 +0300

    "tests"

--- test_002/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите имя ");
-string user_name = Console.ReadLine();
-Console.Write("Привет!, ");
-Console.Write(user_name);
